90723e684c6250877915d38242930aef497d29af
Quorafind <bonopengate@gmail.com>
2022-10-14 23:28:42 +0800

    FEAT: Support Double Click tab

--- main.ts
+++ main.ts
@@ -1,81 +1,58 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, DropdownComponent, MarkdownView, Plugin, PluginSettingTab, Setting, WorkspaceLeaf } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface DblClickTabPluginSettings {
+	defaultDblClickTabBehavior: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: DblClickTabPluginSettings = {
+	defaultDblClickTabBehavior: 'closeTab'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class DblClickTabPlugin extends Plugin {
+	settings: DblClickTabPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.addSettingTab(new DblClickTabSettingTab(this.app, this));
+		this.registerDomEvent(document, 'dblclick', this.dblClickTabBehavior.bind(this));
+	}
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+	dblClickTabBehavior(evt: MouseEvent) {
+		if (!app.workspace.activeLeaf) return;
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+		const activeLeaf = app.workspace.activeLeaf;
+		const leaves: WorkspaceLeaf[] = [];
+		switch (this.settings.defaultDblClickTabBehavior) {
+			case 'closeTab':
+				if (activeLeaf && (evt.target as HTMLElement).classList.contains("workspace-tab-header-inner-title")) {
+					app.workspace.iterateRootLeaves((leaf) => {
+						if (leaf) leaves.push(leaf);
+					});
+
+					if (leaves.length > 1) activeLeaf.detach();
+					else if (leaves.length === 1) {
+						const state = activeLeaf.getViewState();
+						if (!(state.type === "empty")) {
+							activeLeaf.detach();
+						}
 					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
 				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+				break;
+			case 'splitTab':
+				if (activeLeaf) {
+					const state = activeLeaf.getViewState();
+					if (!(state.type === "empty")) {
+						app.workspace.duplicateLeaf(activeLeaf, "vertical");
+					}
+				}
+				break;
+			case 'renameTabFile':
+				if (activeLeaf && activeLeaf.view instanceof MarkdownView) activeLeaf.setEphemeralState({ rename: "all" })
+				break;
+			default:
+				break;
+		}
 	}
 
 	onunload() {
@@ -91,47 +68,51 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class DblClickTabSettingTab extends PluginSettingTab {
+	plugin: DblClickTabPlugin;
+	private applyDebounceTimer: number = 0;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: DblClickTabPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
+	applySettingsUpdate() {
+		clearTimeout(this.applyDebounceTimer);
+		const plugin = this.plugin;
+		this.applyDebounceTimer = window.setTimeout(() => {
+			plugin.saveSettings();
+		}, 100);
+	}
+
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', { text: 'Double Click Tab' });
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName('Default behavior when double clicking a tab')
+			.setDesc("When you double click a tab title, it will trigger behavior. You can change that behavior here.")
+			.addDropdown(async (drowdown: DropdownComponent) => {
+				drowdown
+					.addOption('closeTab', 'Close Tab')
+					.addOption('renameTabFile', 'Rename File')
+					.addOption('splitTab', 'Split Tab')
+					.setValue(this.plugin.settings.defaultDblClickTabBehavior).onChange(async (value) => {
+					this.plugin.settings.defaultDblClickTabBehavior = value;
+					this.applySettingsUpdate();
+				});
+			});
+
+		this.containerEl.createEl('h2', { text: 'Say Thank You' });
+
+		new Setting(containerEl)
+			.setName('Donate')
+			.setDesc('If you like this plugin, consider donating to support continued development:')
+			.addButton((bt) => {
+				bt.buttonEl.outerHTML = `<a href="https://www.buymeacoffee.com/boninall"><img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=boninall&button_colour=6495ED&font_colour=ffffff&font_family=Inter&outline_colour=000000&coffee_colour=FFDD00"></a>`;
+			});
 	}
 }
